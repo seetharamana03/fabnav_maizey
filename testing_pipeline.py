f706cc55b7b2314cf156ef79df6994ea57986829
Anand Seetharaman <seetharamana03@gmail.com>
2025-11-19 12:01:18 -0500

    commit 11-19 12:01PM

--- testing_pipeline.py
+++ testing_pipeline.py
@@ -1,8 +1,16 @@
 import numpy as np
 import pandas as pd
 
+in_progress = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTcFEHlemJJS6S0lzFuMHlSkv17_Wh3k_3PKJKrMpuHF7PJtDfcYKLvZCg_40iOGWL-plkHC82iRGc4/pub?gid=678653982&single=true&output=csv"
+in_progress_df = pd.read_csv(in_progress)
+
+
 def test_script():
     print("This is a test script")
+    value = in_progress_df.iloc[2, 0]
+    print(value)
 
+if __name__ == "__main__":
+    test_script()
 
-test_script()
+#What’s the best way to machine a pocket for a bearing in aluminum?
